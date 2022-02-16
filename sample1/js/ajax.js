fe5a39b925be1b49f88e528764a4d361949b9ea0
Yuto Ichinose <yuto.0526.sano@gmail.com>
2022-02-16 09:54:12 +0900

    ajaxのサンプル

---
+++ sample1/js/ajax.js
@@ -0,0 +1,36 @@
+(function() {
+    function getData() {
+        document.getElementById("Result").value = "問い合わせ中です…";
+        var data = {
+            "code": document.getElementById("Fruits").value
+        }
+        var json = JSON.stringify(data);
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "./php/ajax.php");
+        xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded;charset=UTF-8");
+        xhr.send(json);
+        xhr.onreadystatechange = function () {
+            try {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        var result = JSON.parse(xhr.response);
+                        document.getElementById("Result").value = result.value == 0 ? "選択してください" : result.value;
+                    } else {
+
+                    }
+                } else {
+                
+                }
+            } catch (e) {
+
+            }
+        };
+    }
+    window.addEventListener('load',(e)=>{
+        console.log(document.getElementById('Fruits'));
+        document.getElementById('Fruits').addEventListener('change',(e)=>{
+            getData()
+
+        })
+    });
+})();
